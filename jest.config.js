28b727964c6a1c76f9bc9c3962cbbe719cd4bcae
Ian Hess <ian.h.hess@gmail.com>
2023-02-13 15:18:27 -0500

    apparently its "it" now, not "test"

---
+++ jest.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  modulePaths: ["<rootDir>"],
+  moduleFileExtensions: ["js", "jsx"],
+};
---
+++ test/index.test.js
@@ -0,0 +1,5 @@
+const test = require("../src/index");
+
+it("Check its working at all", () => {
+  expect(2 + 2).toBe(4);
+});
