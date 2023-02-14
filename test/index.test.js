cce7af9157bdfbb6c406865fece31b51533986d9
Ian Hess <ian.h.hess@gmail.com>
2023-02-14 12:02:44 -0500

    have to figure out to test hit function

--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,35 @@
 const Ship = require("../src/Ship");
 
 it("Creates Ship Object", () => {
-  expect(Ship(5, 0, false)).toEqual({ hits: 0, length: 5, sunk: false });
+  expect(
+    Ship(3, 0, [
+      [1, 1],
+      [1, 2],
+      [1, 3],
+    ])
+  ).toEqual({ hits: 0, id: 1, length: 3, sunk: false });
 });
+it("Creates Ship Object", () => {
+  expect(Ship(1, 0, [[2, 2]])).toEqual({
+    hits: 0,
+    id: 2,
+    length: 1,
+    sunk: false,
+  });
+});
+it("Tests isSunk function", () => {
+  expect(
+    Ship(5, 5, [
+      [3, 1],
+      [3, 2],
+      [3, 3],
+      [3, 4],
+      [3, 5],
+    ])
+  ).toEqual({ hits: 5, id: 3, length: 5, sunk: true });
+});
+// it("tests hit function", () => {
+//   Ship(1, 0, [[1, 2]]);
+//   Ship.hit();
+//   expect(Ship.toEqual({ hits: 1, id: 4, length: 1, sunk: true }));
+// });
