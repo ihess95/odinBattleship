95c9e272014d8018a6f62ae88050a9e2b90642bc
Ian Hess <ian.h.hess@gmail.com>
2023-02-14 16:58:02 -0500

    make grid

--- src/Gameboard.js
+++ src/Gameboard.js
@@ -1,1 +1,13 @@
-function Gameboard
+function Gameboard() {
+  let board = [];
+  for (let x = 0; x < 10; x++) {
+    board[x] = [];
+    for (let y = 0; y < 10; y++) {
+      addCell(x, y);
+    }
+  }
+  return;
+}
+function addCell(x, y) {
+  board[x][y] = cell();
+}
